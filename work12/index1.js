c8916ea04f3e4ba3fa9d05f56bdf88268e2a659e
lizilong2011 <644983771@qq.com>
2019-11-25 20:29:31 +0800

    work12

--- work12/index1.js
+++ work12/index1.js
@@ -2,8 +2,7 @@ let year = new Date().getFullYear()
 
 document.getElementById("cal").innerHTML = calendar(year)
 
-function setCalendar(year) {
-    
+function setCalendar(year){
     document.getElementById('cal').innerHTML = '<div class="box" id="cal"></div>';
     var year = parseInt(year)
     var pre = year-1
@@ -14,7 +13,7 @@ function setCalendar(year) {
     document.getElementById('preyear').setAttribute('data-year', pre);
     document.getElementById('nextyear').setAttribute('data-year', next);
     document.getElementById('curyear').innerText = year
-    document.getElementById('preyear').innerText = "<<" + pre
-    document.getElementById('nextyear').innerText = next + ">>"
+    document.getElementById('preyear').innerText = "<<"+pre
+    document.getElementById('nextyear').innerText = next+ ">>"
     document.getElementById("cal").innerHTML = calendar(year)
 }
