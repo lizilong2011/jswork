9dc3f2ddb02e99bd73cc092125b2e19cd5c40412
lizilong2011 <644983771@qq.com>
2019-12-07 14:17:58 +0800

    work14

--- work14/index.js
+++ work14/index.js
@@ -1,29 +1,15 @@
 
-let endseconds = new Date().getTime() + 15 * 1000;
 let d = h = m = s = 0;
-let id = setInterval(seckill,1000);
+
+let id = setInterval(seckill, 1000);
 
 function seckill(){
     let nowtime = new Date();
-    let remaining = parseInt((endseconds - nowtime.getTime()) / 1000);
-    
-    if (remaining > 0){
-        d = parseInt(remaining / 86400);
-        h = parseInt((remaining / 3600) % 24);
-        m = parseInt((remaining / 60) % 60);
-        s = parseInt(remaining % 60);
-    
-        d = d < 10 ? '0' + d : d;
-        h = h < 10 ? '0' + h : h;
-        m = m < 10 ? '0' + m : m;
-        s = s < 10 ? '0' + s : s;
-
-    }else{
-        clearInterval(id);
-        d = h = m = s = '00';
-    
+    let min = parseInt(nowtime.getMinutes());
+    let sec = parseInt(nowtime.getSeconds());
+    if(min>=0&&sec>=0){
+        s = parseInt(60 - sec);
     }
-
     document.getElementById('d').innerHTML = d + '天';
     document.getElementById('h').innerHTML = h + '时';
     document.getElementById('m').innerHTML = m + '分';
